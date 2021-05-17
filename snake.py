8e485f065aff7850447ef147de36ddeaf84b6ce6
ygorkaua <ygorkaua27@gmail.com>
2021-05-17 00:37:34 -0300

    translating the code and add README.md

--- snake.py
+++ snake.py
@@ -1,16 +1,16 @@
 import pygame
 import random
 
-#inicia as fontes no pygame
+#inicia as fonts no pygame
 pygame.font.init()
 
 # tupla da cor de fundo
-azul = (50, 100, 213)
-laranja = (205, 102, 0)
-verde = (0, 255, 0)
+blue = (50, 100, 213)
+orange = (205, 102, 0)
+green = (0, 255, 0)
 
 # tupla da dimensão da tela
-dimensoes = (600, 600)
+size = (600, 600)
 
 # posição do quadardo (cobrinha)
 x = 300
@@ -20,42 +20,42 @@ y = 300
 d = 20
 
 # posições da cobra
-lista_cobra = [[x, y]]
+snakeList = [[x, y]]
 
 # novas posições do corpo
 dx = 0
 dy = 0
 
 #coordenada aleatória inicial do comida
-x_comida = round(random.randrange(0, 600 -d) / 20) * 20
-y_comida = round(random.randrange(0, 600 -d) / 20) * 20
+xFood = round(random.randrange(0, 600 -d) / 20) * 20
+yFood = round(random.randrange(0, 600 -d) / 20) * 20
 
 # define a velocidade inicial da cobra
-velocidade_cobra = 5
+snakeSpeed = 5
 
 #define a fonte usada na pontuação
-fonte = pygame.font.SysFont("System Bols", 20)
+font = pygame.font.SysFont("System Bols", 20)
 
 # define o tamanho da tela
-tela = pygame.display.set_mode((dimensoes))
+screen = pygame.display.set_mode((size))
 # define o titulo da tela
 pygame.display.set_caption('Snake do Ygor')
 
 # define a cor azul para o fundo
-tela.fill(azul)
+screen.fill(blue)
 
-#define a clock para controlar a quantidade de atualizações da tela
+#define a clock para controlar a quantidade de atualizações da screen
 clock = pygame.time.Clock()
 
-def desenha_cobra(lista_cobra):
+def drawSnake(snakeList):
     #limpa a tela (pinta as outras posições, sem ser a atual de azul)
-    tela.fill(azul)
-    for unidade in lista_cobra:
+    screen.fill(blue)
+    for unity in snakeList:
         #define o posição e posição da cobra
-        pygame.draw.rect(tela, laranja, [unidade[0], unidade[1], d, d])
+        pygame.draw.rect(screen, orange, [unity[0], unity[1], d, d])
 
 # função para movimentar, recebe a posição atual
-def mover_cobra(dx, dy, lista_cobra):
+def moveSnake(dx, dy, snakeList):
     # para escutar as teclas
     for event in pygame.event.get():
         #se apertar a tecla
@@ -75,96 +75,92 @@ def mover_cobra(dx, dy, lista_cobra):
                 dy = d
 
     # adiciona o movimento a posição anterior
-    x_novo = lista_cobra[-1][0] + dx
-    y_novo = lista_cobra[-1][1] + dy
+    xNew = snakeList[-1][0] + dx
+    yNew = snakeList[-1][1] + dy
  
-    lista_cobra.append([x_novo, y_novo])
+    snakeList.append([xNew, yNew])
 
     # apaga a ultima posição
-    del lista_cobra[0]
-
-    # adiciona o movimento a posição anterior
-    # x = x + delta_xs
-    # y = y + delta_y
+    del snakeList[0]
 
     # retorna as novas coordenadas
-    return dx, dy, lista_cobra
+    return dx, dy, snakeList
 
 #verifica se ele comeu a comida
-def verifica_comida(dx, dy, x_comida, y_comida, lista_cobra, velocidade_cobra):
+def verifyFood(dx, dy, xFood, yFood, snakeList, snakeSpeed):
     
-    head = lista_cobra[-1]
+    head = snakeList[-1]
 
-    x_novo = head[0] + dx
-    y_novo = head[1] + dy
+    xNew = head[0] + dx
+    yNew = head[1] + dy
 
     # se a cabeça da cobra estiver na posição da comida
-    if head[0] == x_comida and head[1] == y_comida:
-        lista_cobra.append([x_novo, y_novo])
+    if head[0] == xFood and head[1] == yFood:
+        snakeList.append([xNew, yNew])
         # gera uma nova posição aleatória para a comida
-        x_comida = round(random.randrange(0, 600 -d) / 20) * 20
-        y_comida = round(random.randrange(0, 600 -d) / 20) * 20
-        velocidade_cobra = velocidade_cobra + 1
+        xFood = round(random.randrange(0, 600 -d) / 20) * 20
+        yFood = round(random.randrange(0, 600 -d) / 20) * 20
+        snakeSpeed = snakeSpeed + 1
 
     #desenha a comida
-    pygame.draw.rect(tela, verde, [x_comida, y_comida, d, d])
+    pygame.draw.rect(screen, green, [xFood, yFood, d, d])
 
-    return x_comida, y_comida, lista_cobra, velocidade_cobra
+    return xFood, yFood, snakeList, snakeSpeed
 
 #verifica se bateu na parede
-def verifica_parede(lista_cobra):
-    # define a cabeça como a primeira lista do lista_cobra
-    head = lista_cobra[-1]
+def verifyWall(snakeList):
+    # define a cabeça como a primeira lista do snakeList
+    head = snakeList[-1]
     # define o x e o y da cabeça
     x = head[0]
     y = head[1]
 
-    # verifica se a cabeça não está no range da tela
+    # verifica se a cabeça não está no range da screen
     if x not in range(600) or y not in range (600):
         # raise interrompe a execução
         raise Exception
 
 # verifica se houve colisão da cabeça com o corpo da cobra
-def verifica_colisao(lista_cobra):
-    # define a cabeça como a primeira lista do lista_cobra
-    head = lista_cobra[-1]
+def verifyCollision(snakeList):
+    # define a cabeça como a primeira lista do snakeList
+    head = snakeList[-1]
     #copia a lista da cobra
-    corpo_cobra = lista_cobra.copy()
+    snakeBody = snakeList.copy()
     #deleta a posição da cabeça
-    del corpo_cobra[-1]
+    del snakeBody[-1]
 
     #para as coordenadas do corpo
-    for x, y in corpo_cobra:
+    for x, y in snakeBody:
         #se a cabeça estiver na mesma coordenada que o corpo
         if x == head[0] and y == head[1]:
             # raise interrompe a execução
             raise Exception
 
 #atualiza e imprime a quantidade de pontos
-def atualizar_pontos(lista_cobra):
-    #define os pontos como o tamanho do lista_cobra
-    pts = str(len(lista_cobra))
+def updatePoints(snakeList):
+    #define os pontos como o tamanho do snakeList
+    pts = str(len(snakeList))
     # define o que irá mostrar na pontuação
-    score = fonte.render("Pontuação: " + pts, True, laranja)
+    score = font.render("Pontuação: " + pts, True, orange)
     # coloca a pontuação na tela
-    tela.blit(score, [0,0])
+    screen.blit(score, [0,0])
 
 #enquanto for verdadeiro
 while True:
     #atualiza a tela
     pygame.display.update()
-    clock.tick(velocidade_cobra)
+    clock.tick(snakeSpeed)
     # desenha a cobra
-    desenha_cobra(lista_cobra)
+    drawSnake(snakeList)
     # manda a posição atual da cobra para a função, e com o retorno define a nova posição
-    dx, dy, lista_cobra = mover_cobra(dx, dy, lista_cobra)
+    dx, dy, snakeList = moveSnake(dx, dy, snakeList)
     # manda a posição atual da comida para a função, quando pegamos retorna a nova posição
-    x_comida, y_comida, lista_cobra, velocidade_cobra = verifica_comida(dx, dy, x_comida, y_comida, lista_cobra, velocidade_cobra)
+    xFood, yFood, snakeList, snakeSpeed = verifyFood(dx, dy, xFood, yFood, snakeList, snakeSpeed)
     # verifica se a cobra bateu na parede
-    verifica_parede(lista_cobra)
+    verifyWall(snakeList)
     # verifica se a cobra bateu nela mesma
-    verifica_colisao(lista_cobra)
+    verifyCollision(snakeList)
     #atualiza a quantidade de pontos
-    atualizar_pontos(lista_cobra)
+    updatePoints(snakeList)
     # print para debug
-    print(lista_cobra)
+    print(snakeList)
